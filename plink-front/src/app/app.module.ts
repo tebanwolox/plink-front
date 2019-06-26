496503e99585e64ef31b3d528d73d5208344b12f
tebanwolox <esteban.bedoya@wolox.co>
2019-06-26 10:51:36 -0500

    add list component

--- plink-front/src/app/app.module.ts
+++ plink-front/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
